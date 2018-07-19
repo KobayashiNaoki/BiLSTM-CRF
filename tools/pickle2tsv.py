37880cf4f7ea1ceae663dd709cd425fca7853801
NAOKI KOBAYASHI <id.262.1205@gmail.com>
2018-07-20 01:49:33 +0900

    remove space

--- tools/pickle2tsv.py
+++ tools/pickle2tsv.py
@@ -11,13 +11,23 @@ if __name__ == '__main__':
 
     # convert
     text = []
-    for sentence, tag in data:
+    for sentence, tags in data:
         # change ' '  to '<space>'
-        sentence = map(lambda x: '<space>' if x == ' ' else x, sentence)
-        tag = map(lambda x: '<space>' if x == ' ' else x, tag)
+        #sentence = map(lambda x: '<space>' if x == ' ' else x, sentence)
+        #tags = map(lambda x: '<space>' if x == ' ' else x, tags)
+
+        # remove ' '
+        removed_sentence, removed_tags = [], []
+        for char, tag in zip(sentence, tags):
+            if char != ' ':
+                removed_sentence.append(char)
+                removed_tags.append(tag)
+        sentence = removed_sentence
+        tags = removed_tags
+
         sentence = ' '.join(sentence)
-        tag = ' '.join(tag)
-        text.append('\t'.join([sentence, tag]))
+        tags = ' '.join(tags)
+        text.append('\t'.join([sentence, tags]))
 
     # write
     with open(output_file, 'w') as f:
