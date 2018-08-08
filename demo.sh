76caedfcb3f902b55401650de9a2c02e850dd0d7
NAOKI KOBAYASHI <id.262.1205@gmail.com>
2018-08-08 12:08:27 +0900

    update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-GPUID=1
+GPUID=0
 
 echo 'start preprocess.sh'
-./preprocess.sh
+#./preprocess.sh
 echo 'end preprocess.sh'
 
 echo 'start training model'
-CUDA_VISIBLE_DEVICES=$GPUID python train.py -no_CRF -gpu 0 -epoch 10 -out_dir result_noCRF_epoch10_batchsize512_hidden_size128/ -tensorboard
+CUDA_VISIBLE_DEVICES=$GPUID python train.py -no_CRF -gpu 0 -epoch 10 -out_dir result_noFeature_noCRF_epoch10/ -tensorboard\
+                    -no_features &
+CUDA_VISIBLE_DEVICES=$GPUID python train.py -no_CRF -gpu 0 -epoch 10 -out_dir result_noCRF_epoch10/ -tensorboard &
+CUDA_VISIBLE_DEVICES=$GPUID python train.py -no_CRF -gpu 0 -epoch 10 -out_dir result_expand_noCRF_epoch10/ -tensorboard\
+                    -train_file data/train_expand.txt -valid_file data/valid_expand.txt &
+wait
 echo 'end training model'
